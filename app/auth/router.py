6d40b7a463503a9f56b5cdf81f9ed3d4d4f8dd35
Batonchi <gkok.gkok@yandex.ru>
2024-09-28 10:43:10 +0300

    Добавлена HTML разметка и CSS страницы

--- app/auth/router.py
+++ app/auth/router.py
@@ -9,19 +9,24 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory='app/view')
 
+
 @router.post('/registration')
 def registration(first_name: str, last_name: str, email: str, birth_date: date, password: str):
     user = Users(first_name, last_name, email, birth_date, password)
     UserService.save(user)
 
+
 @router.post('/login')
 def login(email: str, password: str):
     return UserService.find_by_email_and_password(email, password)
+
 
 @router.get('/registration')
 def registration_page(request: Request):
     return templates.TemplateResponse('registration.html', {'request': request})
 
+
 @router.get('/login')
 def login_page(request: Request):
     return templates.TemplateResponse('login.html', {'request': request})
+
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from starlette.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from app.auth.router import router as auth_router
+
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory='app/view')
+
 app.include_router(auth_router)
 app.mount('/static', StaticFiles(directory='app/view/static'))
+
+@app.get('/')
+def main_page(request: Request):
+    return templates.TemplateResponse('main.html', {'request': request})
