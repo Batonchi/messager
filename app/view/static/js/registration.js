aebac38afac0f72506dafc7a34e57ebfac1f09ba
as.evseev <as.evseev@tensor.ru>
2024-10-27 12:24:28 +0400

    Изменена отправка объекта пользователя при регистрации

--- app/view/static/js/registration.js
+++ app/view/static/js/registration.js
@@ -7,23 +7,18 @@ document.getElementById('button_form').addEventListener('click', async (event) =
         alert("Пвроли не совпадают!")
         return
     }
-    const user = {
-        first_name: reg_form.first_name.value,
-        last_name: reg_form.last_name.value,
-        email: reg_form.email.value,
-        birth_date: reg_form.birth_date.value,
-        password: reg_form.password.value,
-    }
+    const formData = new FormData()
+    formData.append('first_name', reg_form.first_name.value)
+    formData.append('last_name', reg_form.last_name.value)
+    formData.append('email', reg_form.email.value)
+    formData.append('birth_date', reg_form.birth_date.value)
+    formData.append('password', reg_form.password.value)
     const avatar = reg_form.photo_of_profile.files[0]
     if (avatar) {
-        user['photo_of_profile'] = avatar
+        formData.append('photo_of_profile', avatar)
     }
-    console.log(user)
     response = await fetch('/registration', {
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
         body: JSON.stringify(user)
     })
     if (response.status == 409) {
