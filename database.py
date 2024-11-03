2096fc59d6d80f2583d5dc443ca0010907c7bf09
as.evseev <as.evseev@tensor.ru>
2024-11-03 11:47:25 +0400

    функция create_database

--- database.py
+++ database.py
@@ -12,3 +12,25 @@ def get_connection():
     )
     cursor = conn.cursor()
     return conn, cursor
+
+def create_database():
+    conn, cursor = get_connection()
+    query = """
+
+    CREATE TABLE IF NOT EXISTS users(
+        user_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        email VARCHAR(50) UNIQUE NOT NULL,
+        birth_date DATE,
+        photo_of_profile UUID,
+        about VARCHAR(200),
+        password text
+    );
+
+
+    
+    """
+    cursor.execute(query)
+    conn.commit()
+    
